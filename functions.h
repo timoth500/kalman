8ef69337468e96a899986d3a74e3c0333ed406d0
TimmyBoy <timoth500@yahoo.com>
2017-09-05 11:43:03 -0400

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,55 @@
+/*
+ * functions.h
+ *
+ *  Created on: Sep 4, 2017
+ *      Author: tim
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <random>
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+void matrixOperate(double a[6][6], double b[6]){
+	for(int i = 0;i<6;i++)
+		for(int j = 0;j<6;j++)
+			b[i] = a[j][i]*b[i];
+}
+
+void vectorAdd(double a[6],double b[6]){
+	for(int i = 0;i<6;i++)
+		a[i] = a[i] + b[i];
+}
+
+void identify(double a[6][6]){
+	for(int i = 0;i<6;i++)
+		a[i][i] = 1.0;
+}
+
+void predictState(double sm[6][6],double s[6], double os[6][6], double u[6], double noise[6]){
+	//do something
+	matrixOperate(sm,s);
+	matrixOperate(os,u);
+	vectorAdd(s,u);
+	vectorAdd(s,noise);
+
+}
+
+void addNoise(double a[6]){
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution(0.0,1.0);
+	for (int i=0; i<6; i++)
+		a[i] = distribution(generator);
+
+}
+
+void seeState(double a[6]){
+	std::cout << "The elements are: " << a[0] << ", "  << a[1] << ", "  << a[2] << ", "  << a[3] << ", "  << a[4] << ", "  << a[5] << std::endl;
+}
+
+
+
+#endif /* FUNCTIONS_H_ */
---
+++ scratch.cpp
@@ -0,0 +1,60 @@
+//============================================================================
+// Name        : scratch.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include "/home/tim/workspace/scratch/src/functions.h"
+#include <random>
+using namespace std;
+
+
+int main() {
+
+	int iterMax = 10;
+	double state[6] = {};
+	double stateMatrix[6][6] = {};
+	double u[6] = {};
+	double obsMatrix[6][6] = {};
+	identify(stateMatrix);
+	identify(obsMatrix);
+
+	double noise[6] = {};
+	addNoise(noise);
+
+	seeState(state);
+
+	for(int i = 0; i < iterMax; i++){
+
+		// Step 1: Predict the state vector
+		cout << "Predict the state vector: " << i << endl;
+		predictState(stateMatrix,state,obsMatrix,u,noise);
+		seeState(state);
+		usleep(1000000);
+
+		// Step 2: Predict the covariance matrix
+		//cout << "Predict the covariance matrix: " << i  << endl;
+		//usleep(1000);
+
+		// Step 3: Calculate the Kalman gain
+		//cout << "Calculate the Kalman gain: " << i  << endl;
+		//usleep(10000);
+
+		// Step 4: Update the state vector
+		//cout << "Update the state vector: " << i  << endl;
+		//usleep(100000);
+
+		// Step 5: Update the covariance matrix
+		//cout << "Update the covariance matrix: " << i  << endl;
+		//usleep(1000000);
+
+
+	}
+	cout << "Finished!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
